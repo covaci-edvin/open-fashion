17974dbb694348c2cc05910f5a9f4074cceec156
Covaci Edvin <edvincov28@gmail.com>
2024-01-24 12:01:37 +0200

    public folder setup

--- app/components/ui/Separator/index.tsx
+++ app/components/ui/Separator/index.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import styles from "./Separator.module.scss";
-import SeparatorSvg from "@/app/assets/svgs/separator.svg";
 
 function Separator({ className = "" }: { className?: string }) {
   return (
     <div className={`${styles.container} ${className}`}>
-      <Image src={SeparatorSvg} height={10} width={125} alt="separator" />
+      <Image
+        src={"/assets/svgs/separator.svg"}
+        height={10}
+        width={125}
+        alt="separator"
+      />
     </div>
   );
 }
