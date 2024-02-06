fdebe04627f3b0133c117056438254f25107f055
Covaci Edvin <edvincov28@gmail.com>
2024-02-06 16:30:32 +0200

    bug-fix: product url bug

--- app/components/ui/Product/types.ts
+++ app/components/ui/Product/types.ts
@@ -1,9 +1,13 @@
 export type ProductVariants = "listview" | "gridview" | "homepage" | "full";
-export type Product = {
+export interface Product {
   id: string;
   name: string;
   shortDescription: string;
   price: number;
   rating: number;
   images: string[];
-};
+}
+
+export interface ProductsProps extends Product {
+  url: string;
+}
