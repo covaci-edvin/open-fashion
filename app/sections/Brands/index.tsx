5e7d3e582e1568bbdd90d8cbaa118dc14e5b0adb
Covaci Edvin <edvincov28@gmail.com>
2024-02-08 12:14:32 +0200

    brands section done

---
+++ app/sections/Brands/index.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./Brands.module.scss";
+import { DUMMY_BRANDS } from "@/dummy-data";
+import Separator from "@/app/components/ui/Separator";
+
+function BrandBox({ svgUrl }: { svgUrl: string }) {
+  return (
+    <Image
+      src={svgUrl}
+      alt="brand"
+      width={20}
+      height={16}
+      className={styles.img}
+    />
+  );
+}
+
+function Brands() {
+  return (
+    <section className={`${styles.section} section section-top-margin `}>
+      <Separator />
+      <div className={`${styles.brands}`}>
+        {DUMMY_BRANDS.map((el, index) => (
+          <BrandBox svgUrl={el} key={index} />
+        ))}
+      </div>
+      <Separator />
+    </section>
+  );
+}
+
+export default Brands;
